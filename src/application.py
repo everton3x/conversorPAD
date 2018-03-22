ff892bc3c0e3fbcaa737a3bc047dfb45f33cc7f9
Everton da Rosa <everton3x@gmail.com>
2018-03-22 11:32:56 -0300

    Adicionado suporte à conversão para Excel XLS e XLSX

--- src/application.py
+++ src/application.py
@@ -22,7 +22,7 @@ class App:
         self.reader = None
         self.writer = None
 
-        self.date_parser = lambda d: datetime.strptime(d, '%d%m%Y').strftime('%d/%m/%Y')
+        self.date_parser = lambda d: datetime.strptime(d, '%d%m%Y')
 
     def run(self):
         ''' Executa o programa
@@ -36,14 +36,28 @@ class App:
         if not os.path.exists(self.gui.source_dir.get()):
             self.gui.show_error('Diretório de origem {} não \
             encontrado!'.format(self.gui.source_dir.get()))
+            return
 
         if not os.path.exists(self.gui.output_dir.get()):
             self.gui.show_error('Diretório de destino {} não \
             encontrado!'.format(self.gui.output_dir.get()))
+            return
 
         if self.gui.output_name.get() == '':
-            self.gui.show_error('Nome do arquivo de sáida {} \
+            self.gui.show_error('Nome do arquivo de saída {} \
             não é válido!'.format(self.gui.output_name.get()))
+            return
+
+        if self.gui.output_format.get() == 'CSV':
+            pass
+        elif self.gui.output_format.get() == 'XLS':
+            pass
+        elif self.gui.output_format.get() == 'XLSX':
+            pass
+        else:
+            self.gui.show_error('O formato de saída {} \
+            não é válido!'.format(self.gui.output_format.get()))
+            return
 
         self.sourceFiles = [os.path.join(self.gui.source_dir.get(), filename)
                             for filename in os.listdir(self.gui.source_dir.get())]
@@ -60,29 +74,16 @@ class App:
                 data = False
             else:
                 data = read_fwf(source, colspecs=layout.colspecs(
-                    self.inprocess), names=layout.names(self.inprocess), skiprows=1, skipfooter=1, dtype=layout.dtypes(self.inprocess), parse_dates=layout.date_cols(self.inprocess), date_parser=self.date_parser)
-                # data = to_datetime(data, format='%d%m%Y')
-                # data.apply(to_datetime, format='%d%m%Y')
-                # data.apply(lambda d: d.strftime('d%/%m%Y'))
-            # type(data)
+                    self.inprocess), names=layout.names(self.inprocess), skiprows=1, skipfooter=1, dtype=layout.dtypes(self.inprocess), parse_dates=layout.date_cols(self.inprocess), date_parser=self.date_parser, converters=layout.converters(self.inprocess))
             if data is not False:
-                # print(csv_data)
-                # salva os dados de acordo com o formato escolhido
                 self._save_output(data)
             else:
-                # print('{} não importado'.format(source))
                 pass
 
             self.processing = os.path.basename(source)
             self.percentage += increment
             self.gui.after(100, self._update_progress)
 
-        # if self.gui.progress.get() < 100:
-        #     self.gui.info.set('{}% processado...'.format(self.gui.progress.get()))
-        #     self.gui.progress.set(self.gui.progress.get() + 1)
-        #     self.gui.after(100, self.convert)
-        # else:
-        #     self.gui.info.set('Conversão terminada!')
         return None
 
     def _update_progress(self):
@@ -93,6 +94,11 @@ class App:
     def _save_output(self, data):
         if self.gui.output_format.get() == 'CSV':
             self._save_to_csv(data)
+        elif self.gui.output_format.get() == 'XLSX':
+            self._save_to_xlsx(data)
+        elif self.gui.output_format.get() == 'XLS':
+            self._save_to_xls(data)
+        
 
     def _save_to_csv(self, data):
         output_path = os.path.join(
@@ -101,6 +107,14 @@ class App:
             os.makedirs(output_path)
 
         data.to_csv(os.path.join(output_path, self.inprocess + '.csv'),
-                    sep=';', encoding=os.device_encoding(1), index=False)
-
-        # print(csv)
+                    sep=';', encoding=os.device_encoding(1), index=False, date_format='%d/%m/%Y', float_format='%.2f', decimal=',')
+    
+    def _save_to_xlsx(self, data):
+        output_path = os.path.join(
+            self.gui.output_dir.get(), self.gui.output_name.get() + '.xlsx')
+        data.to_excel(output_path, index=False, float_format='%.2f', sheet_name=self.inprocess)
+    
+    def _save_to_xls(self, data):
+        output_path = os.path.join(
+            self.gui.output_dir.get(), self.gui.output_name.get() + '.xls')
+        data.to_excel(output_path, index=False, float_format='%.2f', sheet_name=self.inprocess)
