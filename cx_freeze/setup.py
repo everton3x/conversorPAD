e8aeeb07c816552328bdb07dd63794ad4fd120bf
Everton da Rosa <everton3x@gmail.com>
2018-04-19 13:54:43 -0300

    Correções e geração de distribuição MSI para Windows

--- cx_freeze/setup.py
+++ cx_freeze/setup.py
@@ -8,15 +8,20 @@ from  cx_Freeze import setup, Executable
 Permite chamar python setup.py build desde qualquer diretório'''
 os.chdir(os.path.dirname(__file__))
 
-''' Previne erro de TCL/TK não encontrado. '''
-#os.environ['TCL_LIBRARY'] = r'C:\Program Files (x86)\Python36-32\tcl\tcl8.6'
-#os.environ['TK_LIBRARY'] = r'C:\Program Files (x86)\Python36-32\tcl\tk8.6'
-
-#base_directory = r'E:\python-virtualenv\conversorPAD'
-base_directory = r'/home/everton/conversorPAD/'
+if sys.platform == 'win32':
+	''' Previne erro de TCL/TK não encontrado. '''
+	os.environ['TCL_LIBRARY'] = r'C:\Users\Everton\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+	os.environ['TK_LIBRARY'] = r'C:\Users\Everton\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
+	base_directory = r'C:\Users\Everton\conversorPAD'
+	
+	exe_name = 'conversor.exe'
+else:
+	base_directory = r'/home/everton/conversorPAD/'
+	exe_name = 'conversor.bin'
+	base = None
+	
 src_directory = os.path.join(base_directory, 'src')
 freeze_directory = os.path.join(base_directory, 'cx_freeze')
-base = None
 
 if sys.platform == 'win32':
     base = 'Win32GUI'
@@ -24,12 +29,12 @@ if sys.platform == 'win32':
 setup(
     name='Conversor do PAD',
     description='Um conversor de layout para os arquivos TXT do PAD',
-    version='1.1',
+    version='1.2',
     executables=[Executable(
         os.path.join(src_directory, 'conversor.py'),
         base=base,
         icon=os.path.join(freeze_directory, 'conversor.ico'),
-        targetName='conversor.exe',
+        targetName=exe_name,
         shortcutName='Conversor do PAD')
     ],
     options = {
@@ -42,9 +47,9 @@ setup(
             'optimize': 2,
             'include_files': [
                 os.path.join(freeze_directory, 'conversor.ico'),
-#                r'C:\Program Files (x86)\Python36-32\DLLs\tcl86t.dll',# Previne erros relacionados a falta do TCL
-#                r'C:\Program Files (x86)\Python36-32\DLLs\tk86t.dll',# Previne erros relacionados a falta do TK
-#                r'C:\Program Files (x86)\Python36-32\DLLs\sqlite3.dll',# Previne erros relacionados a falta do SQLite3
+                os.path.join(freeze_directory, 'tcl86t.dll'),
+                os.path.join(freeze_directory, 'tk86t.dll'),
+                os.path.join(freeze_directory, 'sqlite3.dll'),
             ],
             'path': sys.path + [src_directory]
         }
